67a99e606fecf075b23437f2b6db45bfe3a70f40
Don Denton <don@happycollision.com>
2018-12-26 20:58:38 -0500

    Use slick for slideshow...
    http://kenwheeler.github.io/slick/

--- js/foot.js
+++ js/foot.js
@@ -4,4 +4,16 @@
 $(document).ready(function(){
   {% include_relative _js/main-nav.js %}
   {% include_relative _js/video-considerations.js %}
+
+  $('.slideshow').slick({
+    infinite: true,
+    autoplaySpeed: 3000,
+    autoplay: true,
+    fade: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    cssEase: 'linear',
+    lazyLoad: 'ondemand',
+    mobileFirst: true,
+  })
 });
