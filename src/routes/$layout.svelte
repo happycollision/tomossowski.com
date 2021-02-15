b72f796b7d5c29dd6c62671c1fb6cd4c877a808b
Don Denton <don@happycollision.com>
2021-02-15 12:56:08 -0500

    Add header

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,5 +1,27 @@
 <script>
+  import { page } from "$app/stores"
+  import MainNav from "$components/MainNav.svelte"
+
   import "./_global.pcss"
+
+  $: path = $page.path
 </script>
+
+<header>
+  <h1 class="hidden">Tom Ossowski</h1>
+  <div class="w-11/12 mx-auto my-1 max-w-xl">
+    <svg viewBox="0 0 113 14" class="font-black">
+      <text x="0" y="13">Tom Ossowski</text>
+    </svg>
+  </div>
+
+  <ul class="flex justify-center space-x-4">
+    <li>director</li>
+    <li>music director</li>
+    <li>artistic director</li>
+  </ul>
+</header>
+
+<MainNav {path} />
 
 <slot />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,6 +18,7 @@ module.exports = {
       keyframes: true,
     },
   },
+  darkMode: "media",
   theme: {
     extend: {},
   },
