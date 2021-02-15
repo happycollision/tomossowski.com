a6ecc57ad0a7960ce6a1cce1f24a699225f5393b
Don Denton <don@happycollision.com>
2021-02-15 11:44:27 -0500

    Add tailwind
    1. npx apply svelte-add/postcss
    2. npx apply svelte-add/tailwindcss
    3. npm i
    
    Then adjust Counter.svelte a bit

--- snowpack.config.cjs
+++ snowpack.config.cjs
@@ -1,7 +1,18 @@
 // Consult https://www.snowpack.dev to learn about these options
 module.exports = {
 	extends: '@sveltejs/snowpack-config',
-	plugins: ['@snowpack/plugin-typescript'],
+	plugins: [
+		[
+			'@snowpack/plugin-build-script',
+			{
+				cmd: "postcss",
+				input: [".css", ".pcss"],
+				output: [".css"],
+			}
+		],
+	
+		'@snowpack/plugin-typescript'
+	],
 	mount: {
 		'src/components': '/_components'
 	},
--- src/components/Counter.svelte
+++ src/components/Counter.svelte
@@ -6,29 +6,22 @@
 	};
 </script>
 
-<button on:click={increment}>
+<button class="py-4 px-8 text-red-500 bg-red-500 bg-opacity-10 rounded-full border-2 border-transparent outline-none w-48 focus:border-opacity-100"
+	on:click={increment}
+	>
 	Clicks: {count}
 </button>
 
 <style>
 	button {
-		font-family: inherit;
-		font-size: inherit;
-		padding: 1em 2em;
-		color: #ff3e00;
-		background-color: rgba(255, 62, 0, 0.1);
-		border-radius: 2em;
-		border: 2px solid rgba(255, 62, 0, 0);
-		outline: none;
-		width: 200px;
 		font-variant-numeric: tabular-nums;
 	}
 
 	button:focus {
-		border: 2px solid #ff3e00;
+		@apply border-opacity-100;
 	}
 
 	button:active {
-		background-color: rgba(255, 62, 0, 0.2);
+		@apply bg-red-500 bg-opacity-20;
 	}
 </style>
