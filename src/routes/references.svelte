374228ab4d55b4ba21bad37db1ba58e443feacdf
Don Denton <don@happycollision.com>
2021-02-15 15:25:52 -0500

    Add references page content

---
+++ src/routes/references.svelte
@@ -0,0 +1,51 @@
+<script>
+  import Markdown from "$components/Markdown.svelte"
+</script>
+
+<div class="max-w-xl m-auto">
+  <Markdown
+    source={`
+__Martha Cooper__  
+Associate Professor Costume Design  
+<a class="link" href="mailto:mhcooper@fsu.edu">mhcooper@fsu.edu</a>  
+850-644-6758  
+Florida State University  
+Room 239 FAB  
+530 W Call Street  
+PO Box 3061160  
+Tallahassee FL 32306-1160
+
+__Kris Salata__  
+<a class="link" href="mailto:ksalata@fsu.edu">ksalata@fsu.edu</a>  
+415-810-4439  
+Florida State University  
+Room 239 FAB  
+530 W Call Street  
+PO Box 3061160  
+Tallahassee FL 32306-1160
+
+__Donald Fox__  
+<a class="link" href="mailto:dfoxlighting@gmail.com">dfoxlighting@gmail.com</a>  
+509-264-8872  
+DonFoxDesigns.com  
+Don Fox Designs, LLC  
+28 Columbia Siding Rd.  
+Rock Island, WA 98850
+
+__Wally Seiler__  
+President of the Board of Directors  
+<a class="link" href="mailto:wseiler@charter.net">wseiler@charter.net</a>  
+(308) 760-4693  
+(308) 762-4693  
+Post Playhouse  
+1208 Laramie Avenue  
+Alliance, NE 69301
+
+__Gavin Mayer__  
+<a class="link" href="mailto:gavinmayer@gmail.com">gavinmayer@gmail.com</a>  
+303-210-8724  
+9907 W 53rd Pl.  
+Arvada, CO 80002
+`}
+  />
+</div>
