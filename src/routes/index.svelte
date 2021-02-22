8e0a1252e5b1edaaab306cc6bdcdf5476cee4a14
Don Denton <don@happycollision.com>
2021-02-21 23:47:54 -0500

    Images slideshow for Post 2018

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,54 +1,14 @@
 <script>
-  import Counter from "$components/Counter.svelte"
+  import Post2018, {
+    title as title1,
+  } from "./news/2018-12-26-post-2018-slideshow.svelte"
 </script>
 
 <main>
-  <h1>Hello world!</h1>
-
-  <Counter />
-  <p>
-    Visit the <a class="text-blue-600 underline" href="https://svelte.dev"
-      >svelte.dev</a
-    > to learn how to build Svelte apps.
-  </p>
+  <article class="my-8">
+    <h2 class="text-2xl my-4">
+      {title1}
+    </h2>
+    <Post2018 />
+  </article>
 </main>
-
-<style>
-  :root {
-    /* Tailwind's creator recommends against @apply.
-		This is all just proof that it works in your Svelte style blocks. */
-    @apply font-sans;
-  }
-
-  main {
-    @apply text-center;
-    @apply p-4;
-    @apply mx-auto;
-  }
-
-  h1 {
-    @apply text-red-600;
-    @apply uppercase;
-    @apply text-6xl;
-    @apply font-thin;
-    @apply leading-tight;
-    @apply my-16 mx-auto;
-    @apply max-w-xs;
-  }
-
-  p {
-    @apply max-w-xs;
-    @apply my-8 mx-auto;
-    @apply leading-snug;
-  }
-
-  @screen sm {
-    h1 {
-      @apply max-w-none;
-    }
-
-    p {
-      @apply max-w-none;
-    }
-  }
-</style>
