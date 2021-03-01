a584fb5ea00993d00601ffb88cf609a0dc31365d
Don Denton <don@happycollision.com>
2021-02-28 23:03:09 -0500

    Play button should always be white

--- src/components/portfolio/PlayPausePortfolioBtn.svelte
+++ src/components/portfolio/PlayPausePortfolioBtn.svelte
@@ -6,5 +6,5 @@
 <PlayPauseBtn
   on:click
   {name}
-  class="fill-current absolute bottom-2 left-2 w-1/4 opacity-40 group-hover:opacity-60"
+  class="fill-current text-white absolute bottom-2 left-2 w-1/4 opacity-40 group-hover:opacity-60"
 />
