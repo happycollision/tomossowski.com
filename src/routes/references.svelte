9416e82385c9633c5d8546313c4897e64740ff68
Don Denton <don@happycollision.com>
2021-05-10 06:17:44 -0500

    Update references

--- src/routes/references.svelte
+++ src/routes/references.svelte
@@ -5,16 +5,6 @@
 <div class="max-w-xl m-auto">
   <Markdown
     source={`
-__Martha Cooper__  
-Associate Professor Costume Design  
-[mhcooper@fsu.edu](mailto:mhcooper@fsu.edu)  
-850-644-6758  
-Florida State University  
-Room 239 FAB  
-530 W Call Street  
-PO Box 3061160  
-Tallahassee FL 32306-1160
-
 __Kris Salata__  
 [ksalata@fsu.edu](mailto:ksalata@fsu.edu)  
 415-810-4439  
@@ -33,19 +23,12 @@ Don Fox Designs, LLC
 Rock Island, WA 98850
 
 __Wally Seiler__  
-President of the Board of Directors  
+President of the Board of Directors, Post Playhouse  
 [wseiler@charter.net](mailto:wseiler@charter.net)  
 (308) 760-4693  
 (308) 762-4693  
-Post Playhouse  
 1208 Laramie Avenue  
 Alliance, NE 69301
-
-__Gavin Mayer__  
-[gavinmayer@gmail.com](mailto:gavinmayer@gmail.com)  
-303-210-8724  
-9907 W 53rd Pl.  
-Arvada, CO 80002
 `}
   />
 </div>
